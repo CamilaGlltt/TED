06e4d8d59f5998a54fa955627832ea86bb49d2a1
CamilaGlltt <camilagallettif@gmail.com>
2024-06-26 19:56:06 -0300

    Create Pessoa.cs

---
+++ Entities/Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+using Faculdade.Enums;
+
+namespace Faculdade.Entities
+{
+    public abstract class Pessoa : Entidade
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Sexo Sexo { get; set; }
+        public EstadoCivil EstadoCivil { get; set; }
+        public string CPF { get; set; }
+    }
+}
