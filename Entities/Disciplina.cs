66a4e0d4eebc0d161e301aec2316b240f0de95e2
CamilaGlltt <camilagallettif@gmail.com>
2024-06-26 20:15:26 -0300

    Update Disciplina.cs

--- Entities/Disciplina.cs
+++ Entities/Disciplina.cs
@@ -2,10 +2,14 @@ namespace Faculdade.Entities
 {
     public class Disciplina
     {
+        public class Disciplina : Entidade
         public int DisciplinaId { get; set; }
         public string Nome { get; set; }
         public TipoDisciplina Tipo { get; set; }
         public int CursoId { get; set; }
         public Curso Curso { get; set; }
+        public int ProfessorId { get; set; }
+
+    public List<Matricula> Matriculas { get; } = new();
     }
 }
