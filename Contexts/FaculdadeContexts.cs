ad6f0cff2440b0fd5691a7ca13b092710369659a
CamilaGlltt <camilagallettif@gmail.com>
2024-06-26 19:42:30 -0300

    Create FaculdadeContexts.cs

---
+++ Contexts/FaculdadeContexts.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Faculdade.Entities;
+using System.Globalization;
+
+namespace Faculdade.Contexts
+{
+    public class SapiensContext : DbContext
+    {
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Professor> Professores { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Matricula> Matriculas { get; set; }
+
+        private string caminhoDb;
+
+        public SapiensContext()
+        {
+            var folder = Environment.SpecialFolder.MyDocuments;
+            var path = Environment.GetFolderPath(folder);
+            caminhoDb = Path.Join(path, "sapiens.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+           => options.UseSqlite($"Data Source={caminhoDb}");
+    }
+}
